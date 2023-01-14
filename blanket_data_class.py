6a096a982812b86a75d2f6245f6af969c980c458
Aleksandra Lis <31763332+olalis@users.noreply.github.com>
2023-01-14 22:09:16 +0100

    Update blanket_data_class.py

--- blanket_data_class.py
+++ blanket_data_class.py
@@ -1,16 +1,17 @@
 import serial
+import struct
 
 class BlanketData:
-    def __init__(self, data_frame: str) -> None:
-        temp_blanket_data_list: list[float, bool] = self.proces_data(data_frame)
+    def __init__(self, blanket_data_list) -> None:
+        # temp_blanket_data_list: list[float, bool] = self.proces_data(data_frame)
 
-        self.thermometr1: float = temp_blanket_data_list[0]
-        self.thermometr2: float = temp_blanket_data_list[1]
-        self.thermometr3: float = temp_blanket_data_list[2]
-        self.barometer: float = temp_blanket_data_list[3]
-        self.air_humidity: float = temp_blanket_data_list[4]
-        self.pulse: float = temp_blanket_data_list[5]
-        self.help_me_button: bool = temp_blanket_data_list[6]
+        self.thermometr1: float = blanket_data_list[0]
+        self.thermometr2: float = blanket_data_list[1]
+        self.thermometr3: float = blanket_data_list[2]
+        self.barometer: float = blanket_data_list[3]
+        self.air_humidity: float = blanket_data_list[4]
+        self.pulse: float = blanket_data_list[5]
+        self.help_me_button: bool = blanket_data_list[6]
 
         
 
@@ -19,6 +20,17 @@ class BlanketData:
 
         #return [temp1, temp2, temp3, bar, hum, pulse, help]
 
+    def __str__(self):
+        text = "Dane pobrane z kocyka:\n"
+        text += f"Termometr1: {self.thermometr1}"
+        text += f"Termometr2: {self.thermometr2}"
+        text += f"Termometr3: {self.thermometr3}"
+        text += f"Barometr:   {self.barometer}"
+        text += f"Wilgotność: {self.air_humidity}"
+        text += f"Puls:       {self.pulse}"
+        text += f"HelpMe:     {self.help_me_button}"
+
+        return text
     
     def process_temperature(self):
         pass
@@ -47,13 +59,31 @@ class BlanketData:
         if self.pulse >= treshold_pulse:
             pass
 
-        
+class ADCdata:
+    def __init__(self) -> None:
+        pass
+
+def processData(val: bytes) -> list:
+    # val = b'\xaa\xaa\xaa\xaa\x41\xc2\x66\x66\x41'
+    temp_blanket_data_list = []
+    if val[0:4] == b'\xaa\xaa\xaa\xaa':
+        temp_blanket_data_list.append(struct.unpack('>f', val[4:8])[0]) #Termomert1
+        temp_blanket_data_list.append(struct.unpack('>f', val[8:12])[0]) #Termomert2
+        temp_blanket_data_list.append(struct.unpack('>f', val[12:16])[0]) #Termomert3
+        temp_blanket_data_list.append(struct.unpack('>f', val[16:20])[0]) #Barometr
+        temp_blanket_data_list.append(struct.unpack('>f', val[20:24])[0]) #Wilgtnosc
+        temp_blanket_data_list.append(struct.unpack('>b', val[24])[0]) #helpMe
+        # temp_blanket_data_list.append(struct.unpack('>f', val[25:])[0]) #ADC
+
+    return temp_blanket_data_list
 
 while 1:
-    with serial.Serial('/dev/ttyACM0', 115200, timeout = None) as ser:
+    with serial.Serial('/dev/ttyS0', 9600, timeout = None) as ser:
         try: 
-            x = ser.read(8)
-            print(str(x))
+            x = ser.read()
+            # print(str(x))
+            data = BlanketData(processData(x))
+            print(data)
+
         except serial.SerialException:
             pass
-
