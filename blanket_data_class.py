7c42804d25992afdff7d5027140fb987884f2ede
olalis <aleksandralis2000@gmail.com>
2023-01-14 14:20:16 +0100

    class init

---
+++ blanket_data_class.py
@@ -0,0 +1,15 @@
+
+class BlanketData:
+    def __init__(self, data_frame: str) -> None:
+        self.thermometr1: float = 
+        self.thermometr2: float = None
+        self.thermometr3: float = None
+        self.barometer: float = None
+        self.air_humidity: float = None
+        self.pulse: float = None
+        self.help_me_button: bool = None
+
+    def proces_data(frame_data):
+        pass
+
+        #return [temp1, temp2, temp3, bar, hum, pulse, help]
