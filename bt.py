4c2d117d3e1fe98d03f0a34acc89b0648f53c7d4
Jakub Mazur <61045292+eSqadron@users.noreply.github.com>
2023-01-15 03:48:35 +0100

    Update bt.py

--- bt.py
+++ bt.py
@@ -2,5 +2,8 @@ from bluetooth import *
 from pprint import pprint
 
 devices = discover_devices()
-print(devices)
+for dev in devices:
+    print(dev)
+    for serv in find_service(address = dev):
+        print(serv)
 
